c7fa3f1bc571474610b27c5bf619d823bc281ea8
metuDein <119444184+metuDein@users.noreply.github.com>
2025-06-05 08:18:26 -0400

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -79,7 +79,7 @@ const page = () => {
   }
 
   useEffect(() => {
-    toast("Please reload the tab before logging in.");
+    toast("Please reload the tab after logging in.");
   }, [])
   
   return (
